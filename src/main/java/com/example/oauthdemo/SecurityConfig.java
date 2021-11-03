33d3ef3e8d555448eda818d65113af7dc69c4fa2
youssef chaker <kikoulolz1youssef@gmail.com>
2021-11-03 11:42:48 +0100

    first

---
+++ src/main/java/com/example/oauthdemo/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.oauthdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloackRoleConverter());
+
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/users/status/check")
+                .hasAuthority("ROLE_dev")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+}
